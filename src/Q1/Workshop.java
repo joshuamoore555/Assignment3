8d447b79cf901e28ac5cf176aa468b6050a3666e
Joshua <joshuamoore555@gmail.com>
2017-12-20 00:58:18 +0000

    reindeers

--- src/Q1/Workshop.java
+++ src/Q1/Workshop.java
@@ -4,8 +4,8 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Workshop {
-    private static AtomicInteger numberOfElves = new AtomicInteger(4);
-    static Semaphore elvesOutside = new Semaphore(numberOfElves.get(), true);
+    private static int numberOfElves = 4;
+    static Semaphore elvesOutside = new Semaphore(numberOfElves, true);
     static Semaphore elvesInside = new Semaphore(0, true);
 
     static Semaphore santaSemaphore = new Semaphore(1, true);
@@ -13,7 +13,7 @@ public class Workshop {
     static Semaphore mutex = new Semaphore(1, true);
 
     public static void main(String args[]) {
-        for(int i = 0; i < numberOfElves.get(); i++){
+        for(int i = 0; i < numberOfElves; i++){
             new Elf("Elf " + i).start();
         }
         new Santa().start();
@@ -28,18 +28,17 @@ public class Workshop {
                 while (true) {
                     mutex.acquire(1);
                     santaSemaphore.acquire(1);
-                    System.out.println("Santa Waiting..."  + elvesInside.availablePermits() + "  " + elvesOutside.availablePermits());
+                    System.out.println("Santa Waiting... "  + elvesInside.availablePermits() + "  " + elvesOutside.availablePermits());
                     reindeerSemaphore.acquire(1); //wait for reindeer to leave
-                    elvesOutside.acquire(numberOfElves.get()); //wait for elves to be outside
+                    elvesOutside.acquire(numberOfElves); //wait for elves to be outside
                     mutex.release(1);
 
-                    System.out.println("Santa Entered"  + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
-                    elvesOutside.release(numberOfElves.get()); //allow elves to enter
-                    reindeerSemaphore.release(1); //allow reindeer to attempt to enter
+                    System.out.println("Santa Entered "  + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
+                    elvesOutside.release(numberOfElves); //allow elves to enter
 
-
+                    System.out.println("Santa left "  + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
                     santaSemaphore.release(1);//santa leaves
-                    System.out.println("Santa left"  + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
+                    reindeerSemaphore.release(1); //allow reindeer to attempt to enter
                     Thread.sleep(3000);
 
 
@@ -70,6 +69,7 @@ public class Workshop {
                     elvesOutside.release(1);
                     System.out.println(name + " left " + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
 
+
                     Thread.sleep(3000);
                 }
             }catch (InterruptedException e) {
@@ -92,7 +92,7 @@ public class Workshop {
                     //attempt to enter
                     mutex.acquire(1);
                     reindeerSemaphore.acquire(1);
-                    System.out.println(name + " Waiting..."  + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
+                    System.out.println(name + " Waiting... "  + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
                     santaSemaphore.acquire(1); //wait for santa to leave
                     System.out.println("Santa isn't there!"  + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
                     elvesInside.acquire(4); //wait for 4 elves to enter
@@ -106,7 +106,7 @@ public class Workshop {
 
                     reindeerSemaphore.release(1);
                     elvesInside.release(2);
-                    System.out.println(name + " the Reindeer left." + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
+                    System.out.println(name + " the Reindeer left. " + elvesInside.availablePermits()+ "  " + elvesOutside.availablePermits());
 
                     Thread.sleep(3000);
 
